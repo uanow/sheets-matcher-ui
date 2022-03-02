afe85f6260628486fbc8d72bac0ca960bfbcf6dd
tchief <cyber.tchief@gmail.com>
2022-03-02 18:05:48 +0200

    Removed dependency.

--- src/match/index.ts
+++ src/match/index.ts
@@ -1,12 +1,13 @@
-import { Context } from "grammy";
-import { getMatches } from "./match";
-import { saveMatchesToRequestSheet } from "./sheets";
-import { Match, matchesToString } from "./types";
+import { getMatches } from './match';
+import { saveMatchesToRequestSheet } from './sheets';
+import { Match, matchesToString } from './types';
 
-const match = async (ctx: Context, rewrite = false): Promise<void> => {
-    const matches: Match[] = await getMatches();
-    if (rewrite) await saveMatchesToRequestSheet(matches);
-    await ctx.reply(matches.length ? `request: proposals\n\n${matchesToString(matches)}` : "No matches found yet.");
+const match = async (rewrite = false): Promise<string> => {
+  const matches: Match[] = await getMatches();
+  if (rewrite) await saveMatchesToRequestSheet(matches);
+  return matches.length
+    ? `request: proposals\n\n${matchesToString(matches)}`
+    : 'No matches found yet.';
 };
 
 export default match;

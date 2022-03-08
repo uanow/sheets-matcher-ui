3ebbe04f2a50311325b824dca6b79402b43f73be
tchief <cyber.tchief@gmail.com>
2022-03-08 20:49:29 +0200

    Sending columns from bot depends on env variable.

--- src/match/details.ts
+++ src/match/details.ts
@@ -18,22 +18,23 @@ export const sendDetailsToChat = async (matchRequest: MatchRequest): Promise<num
 
   const chatId = !!matchRequest.chatId ? matchRequest.chatId : process.env.CHAT_ID;
 
-  console.log({ message, chatId, token: process.env.BOT_TOKEN?.[0] });
+  // console.log({ message, chatId, token: process.env.BOT_TOKEN?.[0] });
 
   const response = await fetch(
     `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=${chatId}&text=${message}`
   );
 
-  console.log({ response });
+  // console.log({ response });
 
   return response.status;
 };
 
 const matchToConnectString = (request: any, proposal: any, matchRequest: MatchRequest) => {
   // TODO: Potentially exposes columns hidden from operators, needs implementing chats approval.
-  const columnsSendToChat = matchRequest.columnsSendToChat?.split(',').filter(Boolean) ?? [
-    'telegram',
-  ];
+  const defaultColumns = ['telegram'];
+  const columnsSendToChat = !!process.env.SEND_COLUMNS
+    ? matchRequest.columnsSendToChat?.split(',').filter(Boolean) ?? defaultColumns
+    : defaultColumns;
   const details = columnsSendToChat.map((c) => `${request[c]} <-> ${proposal[c]}`).join('%0A');
   // console.log({ columnsSendToChat, details, request, proposal });
   return `Request №${request.rowNumber} <-> Proposal №${proposal.rowNumber}%0A${details}`;

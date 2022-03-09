e7acb175c958a74448868c709a32ca7e80130159
tchief <cyber.tchief@gmail.com>
2022-03-09 21:41:23 +0200

    Added airtable vs sheets pick func.

--- src/match/match.ts
+++ src/match/match.ts
@@ -1,11 +1,14 @@
+import { getAirtableRetrieveFuncs } from './airtable';
 import getGenericMatchFuncs from './generic';
 import getRefugeeMatchFuncs from './refugee';
-import { getProposals, getRequests } from './sheets';
+import { getSheetsRetrieveFuncs } from './sheets';
 import { Match, matchesToString, MatchRequest } from './types';
 
 export const getMatches = async (matchRequest: MatchRequest): Promise<Match[]> => {
   const { filterRequests, filterProposals, match, mapRowToRequest, mapRowToProposal } =
     getMatchFuncs(matchRequest);
+
+  const { getRequests, getProposals } = getRetrieveFuncs(matchRequest);
 
   const requests = (await getRequests(matchRequest, mapRowToRequest)).filter(filterRequests);
   const proposals = (await getProposals(matchRequest, mapRowToProposal)).filter(filterProposals);
@@ -19,7 +22,7 @@ export const getMatches = async (matchRequest: MatchRequest): Promise<Match[]> =
     }))
     .filter((match) => match.proposalIds.length > 0);
 
-  //console.log({ proposals, requests, matches: matchesToString(matches) });
+  // console.log({ proposals, requests, matches: matchesToString(matches) });
 
   return matches;
 };
@@ -28,3 +31,6 @@ export const getMatchFuncs = (matchRequest: MatchRequest) =>
   matchRequest.matchType === 'un-refugee'
     ? getRefugeeMatchFuncs
     : getGenericMatchFuncs(matchRequest);
+
+export const getRetrieveFuncs = (matchRequest: MatchRequest) =>
+  matchRequest.sourceType === 'airtable' ? getAirtableRetrieveFuncs : getSheetsRetrieveFuncs;
